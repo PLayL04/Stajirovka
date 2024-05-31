187f99bb2531640fab2793c0478bd6161f738761
PLayL04 <playl04@yandex.ru>
2024-05-31 19:12:00 +0700

    Player Movement & Look-At-Target System

---
+++ Assets/Scripts/LookAtTargetSystem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LookAtTargetSystem
+{
+    private readonly Transform transform;
+
+    public LookAtTargetSystem(Transform transform)
+    {
+        this.transform = transform;
+    }
+
+    public void LookAt(Vector3 target)
+    {
+        float angleRad = Mathf.Atan2(target.y - transform.position.y, target.x - transform.position.x);
+        float angleDeg = angleRad * Mathf.Rad2Deg - 90f;
+        transform.localRotation = Quaternion.Euler(0, 0, angleDeg);
+    }
+}
